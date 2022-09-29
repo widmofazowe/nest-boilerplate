b7910ad80b4085101ae1c247ee34791d5cea7ba1
widmo <widomski.pawel@gmail.com>
2022-09-29 09:47:35 +0200

    fix

--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -1,4 +1,4 @@
+export { AbstractEntity } from './abstract.entity';
 export { CoreModule } from './core.module';
 export { CoreService } from './core.service';
 export { InjectCoreService } from './inject.decorator';
-export { AbstractEntity } from './abstract.entity';
